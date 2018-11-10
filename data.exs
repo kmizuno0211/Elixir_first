e4e1682ca62f9ca45dcd94ec528884c217935fcd
Kento Mizuno <kmizuno0211@Kentos-MacBook-Pro.local>
2018-11-11 04:56:17 +0900

    Update data.exs

--- data.exs
+++ data.exs
@@ -1,5 +1,9 @@
+iex> data |> Enum.map( fn( record ) -> %{ "name" => record[ "name"], "age" => record[ "age" ] } end)
+
+
+
 iex> data = [
   %{ "name" => "みずの", "age" => 29, "team" => "ArtRiteLacy.jp", "position" => "代表、音楽家" },
-  %{ "name" => "たろう", "age" => ??, "team" => "ArtRitelacy.jp", "position" => "デザイナー" },
+  %{ "name" => "たろう", "age" => ??, "team" => "ArtRiteLacy.jp", "position" => "デザイナー" },
   %{ "name" => "まめも", "age" => ??, "team" => "??", "position" => "ご支援、みそランチャー" },
 ]
